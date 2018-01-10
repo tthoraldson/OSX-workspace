216e5126ebea109967b625a273d286a265d50147
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2018-01-10 09:00:04 -0600

    LAME COMMIT tm

--- main.pl
+++ main.pl
@@ -5,14 +5,15 @@ $|=1;
 
 sub main {
   workspace_ascii();
-  check_for_updates();
+  # check_for_updates();
+  conda();
 
   # choose( [ 'Press ENTER to continue' ], { prompt => '' } );    # no choice
 }
 
 sub workspace_ascii {
   print "OSX Workspace - Theresa Thoraldson\n";
-  print "Last Updated: 9/23/2017\n\n"
+  print "Last Updated: 1/9/2018\n\n"
 }
 
 sub check_for_updates {
@@ -23,7 +24,12 @@ sub check_for_updates {
   if ($choice eq "yes") {
     print "Starting TMUX\n";
     exec('tmux');
-  } 
+  }
+}
+
+sub conda {
+  print "Exporting Conda File Path...";
+  system("export PATH=~/anaconda3/bin:\$PATH\n")
 }
 
 main();
